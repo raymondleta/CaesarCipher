89f82103e3fd41416151ae13294dd8352420bef6
System Administrator <root@Owners-MacBook-Pro.local>
2019-04-06 23:35:27 +0300

    edit main class styling

--- src/test/java/CaesarCipher/DecryptTest.java
+++ src/test/java/CaesarCipher/DecryptTest.java
@@ -8,6 +8,7 @@ public class DecryptTest {
     @Test
     public void testDencryptClass() {
         Decrypt classUnderTest = new Decrypt();
-        assertNotNull("app shoule convert j to h when shift is 2", classUnderTest.dencryptClass("jsudo ", 2));
+        assertNotNull("app shoule convert j to h when shift is 2", classUnderTest.dencryptClass("j ", 2));
+        assertNotNull("app shoule convert jgnnq to hello  when shift is 2", classUnderTest.dencryptClass("jgnnq", 2));
     }
 }
--- src/test/java/CaesarCipher/EncryptTest.java
+++ src/test/java/CaesarCipher/EncryptTest.java
@@ -7,5 +7,6 @@ public class EncryptTest {
     @Test public void testEncryptClass() {
         Encrypt classUnderTest = new Encrypt();
         assertNotNull("app shoule convert a to c when shift is 2", classUnderTest.encryptClass("A", 2));
+        assertNotNull("app shoule convert hello to jgnnq when shift is 2", classUnderTest.encryptClass("A", 2));
     }
 }
