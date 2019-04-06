36176021de8aed00f966c0ca3d3ddc9b1e6703c4
System Administrator <root@Owners-MacBook-Pro.local>
2019-04-06 21:40:00 +0300

    test decrypt class

--- src/test/java/CaesarCipher/DecryptTest.java
+++ src/test/java/CaesarCipher/DecryptTest.java
@@ -1,4 +1,13 @@
 package CaesarCipher;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
 public class DecryptTest {
+    @Test
+    public void testDencryptClass() {
+        Decrypt classUnderTest = new Decrypt();
+        assertNotNull("app shoule convert j to h when shift is 2", classUnderTest.dencryptClass("j", 2));
+    }
 }
