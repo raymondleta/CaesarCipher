b813a7ccaf72f684e6b02b7f7430d70d061839e8
System Administrator <root@Owners-MacBook-Pro.local>
2019-04-06 21:24:21 +0300

    add decrypt algorithim that is a reverse of the encrypt class algoorithm

--- src/main/java/CaesarCipher/Decrypt.java
+++ src/main/java/CaesarCipher/Decrypt.java
@@ -1,5 +1,25 @@
 package CaesarCipher;
 
 public class Decrypt {
+    public static StringBuilder dencryptClass(String message, int shift){
+        StringBuilder dencryptedResult = new StringBuilder();
+
+        for (int i=0; i<message.length(); i++){
+            if (Character.isUpperCase(message.charAt(i)))
+            {
+                char character = (char)(((int)message.charAt(i) +
+                        shift - 65) % 26 + 65);
+                dencryptedResult.append(character);
+            }
+            else
+            {
+                char character = (char)(((int)message.charAt(i) +
+                        shift - 97) % 26 + 97);
+                dencryptedResult.append(character);
+            }
+
+        }
+        return dencryptedResult;
+    }
 
 }
